540e0496e9959a209dd720db381d643b589af567
Luis Martinez <mr.luiswebmaster@gmail.com>
2023-06-09 01:01:27 -0500

    Initial commit

---
+++ src/api/experiencie/routes/experiencie.ts
@@ -0,0 +1,7 @@
+/**
+ * experiencie router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::experiencie.experiencie');
---
+++ src/api/experiencie/services/experiencie.ts
@@ -0,0 +1,7 @@
+/**
+ * experiencie service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::experiencie.experiencie');
